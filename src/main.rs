f31b5c118a926153b28883d10e74b3ba739af40f
Erik Scott Korzon <eskorzon@gmail.com>
2023-11-17 23:51:10 -0800

    now with a button

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,32 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use defmt::*;
+// use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{
+    Input,
+    Level,
+    Output,
+    Pull,
+    Speed,
+};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
 
     let mut led = Output::new(p.PB3, Level::High, Speed::Low);
+    let mut button = ExtiInput::new(
+        Input::new(p.PB4, Pull::Down), p.EXTI4
+    );
 
     loop {
-        led.set_high();
-        Timer::after_millis(300).await;
-        led.set_low();
-        Timer::after_millis(300).await;
+        button.wait_for_rising_edge().await;
+        match led.get_output_level() {
+            Level::High => led.set_low(),
+            Level::Low => led.set_high()
+        }
     }
 }
